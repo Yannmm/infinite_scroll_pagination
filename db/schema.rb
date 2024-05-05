44765faa1c9e447a3bc092413bda40bf2c068373
Yan Meng <yannmm@foxmail.com>
2024-05-05 19:26:46 +0800

    scaffold posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_04_002137) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_05_112628) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
