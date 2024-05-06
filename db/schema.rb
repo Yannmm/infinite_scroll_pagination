73a64a4fcac086c098cade81e6fbfea7e65b486b
Rayman Yan <ryan@aligntech.com>
2024-05-06 13:45:33 +0800

    infinite scroll with turbo frame + stream, good

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_05_112628) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_06_045324) do
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_url"
+  end
+
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,4 +12,5 @@
 100.times do 
     Comment.create(body: Faker::Quote.famous_last_words)
     Post.create(title: Faker::Music.band, image_url: Faker::Avatar.image)
+    Artist.create(name: Faker::Artist.name, image_url: Faker::Avatar.image)
 end
